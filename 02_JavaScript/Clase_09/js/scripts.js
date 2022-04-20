a634ffdac262b012cb1c7d6f598581c5077aa3c9
GonzaloZubiri <gonzalozubiri24@gmail.com>
2022-04-19 21:42:46 -0300

    Clase 09  19-04-2022

---
+++ 02_JavaScript/Clase_09/js/scripts.js
@@ -0,0 +1,146 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     /** Obtiene el hero con las imagenes */
+//     const imagenHero = document.querySelector('.hero');
+
+//     /** Arreglo de imagenes con fondo */
+//     const imagenes = ['arriba2.jpg', 'arriba.jpg' ];
+
+//     /**Inicializadores */
+//     let i = 0;
+//     let tiempo = 0;
+//     setInterval( () => {
+//         imagenHero.style.backgroundPositionY =  '-'+tiempo+'px';
+//         // console.log(tiempo);
+//         if(tiempo > 40) {
+//             tiempo = 0;
+//             imagenHero.style.backgroundImage = "url(../img/"+imagenes[i]+")";
+
+//             if(i === imagenes.length - 1) {
+//                 i = 0;
+//             } else {
+//                 i++;
+//             }
+//         }
+
+//         tiempo += 1;
+//     }, 100);
+// });
+
+
+
+// Boton Flotante con el footer
+// const btnFlotante = document.querySelector('.btn-flotante');
+
+// btnFlotante.addEventListener('click', function() {
+
+
+//     const footer = document.querySelector('.footer');
+
+
+//     if( footer.classList.contains('activo') ) {
+//         footer.classList.remove('activo');
+//         this.classList.remove('activo');
+//         this.innerText = 'Idioma y Moneda';
+//     } else {
+//         footer.classList.add('activo');
+//         this.classList.add('activo');
+//         this.innerText = 'X Cerrar';
+//     }
+
+// });
+
+
+
+/*--------------------------------------------------------------------------*/
+
+
+
+
+//captura el evento cuando pasa el cursor sobre el NavBar
+// const nav = document.querySelector("body > div > header > nav");
+// nav.addEventListener("mouseenter", ()=> {
+//     console.log("Entrando en el Nav");
+//     nav.style = "background:blue";
+// });
+
+// nav.addEventListener("mouseout", ()=> {
+//     console.log("Saliendo del nav");
+//     nav.style = "background:red";
+// });
+
+
+// nav.addEventListener("click", ()=> {
+//     console.log("Entrando en el Nav");
+//     nav.style = "background:blue";
+// });
+
+
+// window.addEventListener("scroll", () => {
+//     // const scrollY = window.scrollY;
+//     // // console.log(scrollY);
+//     // if (scrollY >= 1500) {
+//     //     console.log(scrollY);
+//     // }
+
+
+//     const premium = document.querySelector(".premium");
+//     // console.log(premium);
+//     const ubicacion = premium.getBoundingClientRect();
+//     // console.log(ubicacion);
+//     if (ubicacion.top < 760) {
+//         console.log("Elemento visible");
+//     } else {
+//         console.log("No esta visible");
+//     };
+
+// });
+
+
+const input = document.querySelector(".busqueda");
+// console.log(input);
+// input.addEventListener("blur", (e) => {  //Es lo mismo utilizar la letra "e" como parametro que la palabra "event"
+//     // console.log(e);
+//     // console.log("Ejecuta el blur");
+//     e.target.placeholder = "Nuevas modificaciones del placeholder";
+//     e.target.style.background = "red";
+//     e.target.className = "contenedor";
+// });
+
+// input.addEventListener("focus", (e) => {
+//     // console.log(e);
+//     e.target.style.background = "";
+//     e.target.placeholder = "New York, Londres, Roma, Guadalajara";
+//     e.target.className = "";
+// });
+
+
+// Otra forma de hacerlo con funciones (osea llamando a funciones para no tener que hacr tanto codigo repetido en cada AddEventlistener) ES LO MISMO QUE LO DE ARRIBA PERO MAS ORDENADO Y MENOS CODIGO
+
+// input.addEventListener("blur", desenfocar);
+
+// input.addEventListener("focus", enfocar);
+
+// function enfocar (e){
+//     e.target.style.background = "";
+//     e.target.placeholder = "New York, Londres, Roma, Guadalajara";
+//     e.target.className = "";
+// }
+
+// function desenfocar (e){
+//     e.target.placeholder = "Nuevas modificaciones del placeholder";
+//     e.target.style.background = "red";
+//     e.target.className = "contenedor";
+// }
+
+
+
+input.addEventListener("input", (e) => {
+    // console.log(e.target.value);
+    if (e.target.value === "") {
+        console.log("Debe completar el campo");
+        e.target.style.background = "red";
+    }else {
+        e.target.style.background = "";
+    }
+});
