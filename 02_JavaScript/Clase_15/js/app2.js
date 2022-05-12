42b232934007f8bc017440538078e7852b115d51
GonzaloZubiri <gonzalozubiri24@gmail.com>
2022-05-12 00:10:56 -0300

    Clase 15  10-05-22

---
+++ 02_JavaScript/Clase_15/js/app2.js
@@ -0,0 +1,30 @@
+const aplicarDescuento = new Promise((resolve, reject) => {
+    const dto = false
+    if (dto) {
+        resolve('Descuento aplicado')
+    } else {
+        reject('Descuento no aplicado')
+    }
+})
+
+aplicarDescuento
+    .then(res => {
+        console.log(res);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+    
+
+/* Crear con Javascript un código para que se pueda mostrar en el DOM la info que devuelve la promesa */
+
+
+/* console.log(aplicarDescuento);*/
+
+//Existen 3 estados de las promesas
+/* 
+fulfilled -> Cuando la promesa se cumplió
+rejected -> Cuando la promesa no se cumplió
+pending -> Cuando queda en pendiente
+*/
